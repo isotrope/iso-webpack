34b8de0452fc60e80df8cbdc215225776b5fd2e4
Michal Bluma <Z62dTCgT&CDZhD^5>
2019-02-13 17:51:12 -0500

    Added dev-server and fixed sourceMaps

--- webpack.config.js
+++ webpack.config.js
@@ -36,12 +36,12 @@ module.exports = {
 					MiniCssExtractPlugin.loader,
 					{
 						loader : 'css-loader',
-						// options : {
+						options : {
 						// 	importLoaders : 1,
-						// 	sourceMap     : true,
+							sourceMap     : true,
 						// 	url           : false,
 						// 	minimize      : true,
-						// }
+						}
 					},
 					{
 						loader  : 'postcss-loader',
@@ -86,7 +86,23 @@ module.exports = {
 			// both options are optional
 			filename : 'css/style.css',
 		} )
-	]
+	],
+	devServer: {
+		// Display only errors to reduce the amount of output.
+		stats: "errors-only",
+
+		// Parse host and port from env to allow customization.
+		//
+		// If you use Docker, Vagrant or Cloud9, set
+		// host: options.host || "0.0.0.0";
+		//
+		// 0.0.0.0 is available to all network devices
+		// unlike default `localhost`.
+		host: process.env.HOST, // Defaults to `localhost`
+		port: process.env.PORT, // Defaults to 8080
+		open: true, // Open the page in browser
+		writeToDisk: true,
+	},
 
 
 };
